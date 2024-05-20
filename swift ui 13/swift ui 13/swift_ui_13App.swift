96e22c0a2edeb25f9d7bb90b93923ae8ace69ba5
Ilya Kazlou <ilya02k@gmail.com>
2024-05-20 17:22:31 +0300

    swift ui scroll view and bank app

---
+++ swift ui 13/swift ui 13/swift_ui_13App.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_13App.swift
+//  swift ui 13
+//
+//  Created by Ann Dosova on 20.05.24.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
